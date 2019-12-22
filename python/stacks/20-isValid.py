9e20c8281bec39870b188471873bc438fb281670
Brian Ly <thangln1003@gmail.com>
2019-12-22 22:31:17 +0800

    Update codes

---
+++ python/stacks/20-isValid.py
@@ -0,0 +1,56 @@
+
+# 20. Valid Parentheses
+# Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+# An input string is valid if:
+
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+# Note that an empty string is also considered valid.
+
+# Example 1:
+# Input: "()"
+# Output: true
+
+# Example 2:
+# Input: "([)]"
+# Output: false
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # The stack to keep track of opening brackets.
+        stack = []
+
+        # Hash map for keeping track of mappings. This keeps the code very clean.
+        # Also makes adding more types of parenthesis easier
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            # If the character is an closing bracket
+            if char in mapping:
+                # Pop the topmost element from the stack, if it is non empty
+                # Otherwise assign a dummy value of '#' to the top_element variable
+                top_element = stack.pop() if stack else '#'
+
+                # The mapping for the opening bracket in our hash and the top
+                # element of the stack don't match, return False
+                if mapping[char] != top_element:
+                    return False
+            else:
+                # We have an opening bracket, simply push it onto the stack.
+                stack.append(char)
+
+        # In the end, if the stack is empty, then we have a valid expression.
+        # The stack won't be empty for cases like ((()
+        return not stack
+
+
+if __name__ == "__main__":
+    # input = "([)]"
+    input = "[()(){}]"
+
+    s = Solution()
+    result = s.isValid(input)
+
+    print(result)
