926c57f84f4221455c99409b07905058ecda0a7f
Brian Ly <brian.ly@mnltechnology.com>
2019-12-24 11:41:02 +0700

    125. Valid Palidrome

---
+++ python/string/125-isPalindrome.py
@@ -0,0 +1,31 @@
+# 125. Valid Palindrome (Easy)
+# https://leetcode.com/problems/valid-palindrome/
+
+# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+# Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+# Input: "A man, a plan, a canal: Panama"
+# Output: true
+
+# Input: "race a car"
+# Output: false
+
+import string
+
+class Solution:
+    def isPalindrome(self, s:str) -> bool:
+        s = s.lower().replace(" ", "").translate(str.maketrans('', '', string.punctuation))
+
+        return s == s[::-1]
+
+if __name__ == "__main__":
+    # input = "A man, a plan, a canal: Panama"
+    input = "race a car"
+
+    s = Solution()
+    result = s.isPalindrome(input)
+
+    if result:
+        print("True")
+    else:
+        print("False")
---
+++ python/string/5-longestPalindrome.py
@@ -0,0 +1,18 @@
+# 5. Longest Palindromic Substring (Medium)
+# https://leetcode.com/problems/longest-palindromic-substring/solution/
+
+# Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+# Input: "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
+
+# Input:    "abac-dfgd-caba"
+# Output:   "abac-dgfd-caba"
+
+# TODO: Approach 1: Longest Common Substring
+# TODO: Approach 2: Brute Force
+# TODO: Approach 3: Dynamic Programming [O(n*2) & O(n*2)]
+# TODO: Approach 4: Expand Around Center [O(n*2) & O(1)]
+# TODO: Approach 5: Manacher's Algorithm [O(n) & O(1)]
+
