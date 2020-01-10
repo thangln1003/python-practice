106d149b12ce58bb0061b2414cfbf51fe20dea80
Brian Ly <brian.ly@mnltechnology.com>
2020-01-10 18:27:16 +0700

    Implement "347. Top K Frequent Elements"

---
+++ python/2-array/347-topKFrequent.py
@@ -0,0 +1,34 @@
+# 347. Top K Frequent Elements
+# https://leetcode.com/problems/top-k-frequent-elements/
+
+# Given a non-empty array of integers, return the k most frequent elements.
+
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
+# Example 2:
+
+# Input: nums = [1], k = 1
+# Output: [1]
+
+# Note:
+# You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+# Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+# TODO: Approach 1: Heap        
+
+from typing import List
+from collections import Counter
+import heapq
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+
+        return heapq.nlargest(k, count.keys(), key=count.get)
+
+
+if __name__ == "__main__":
+    nums, k = [1, 1, 1, 2, 2, 3], 2
+
+    s = Solution()
+    print(s.topKFrequent(nums, k))
