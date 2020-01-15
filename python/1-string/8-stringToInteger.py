d55371910171b60a65c5343ce56ea5d995091085
Brian Ly <brian.ly@mnltechnology.com>
2020-01-15 18:40:55 +0700

    Update code

---
+++ python/1-string/8-stringToInteger.py
@@ -0,0 +1,11 @@
+# 8. String To Integer (Medium)
+# https://leetcode.com/problems/string-to-integer-atoi/
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        ans = 0
+
+        return ans
+
+if __name__ == "__main__":
+    
--- python/stacks/20-isValidParentheses.py
+++ python/stacks/20-isValidParentheses.py
@@ -1,5 +1,6 @@
 
-# 20. Valid Parentheses
+# 20. Valid Parentheses (Easy)
+# https://leetcode.com/problems/valid-parentheses/
 # Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
 
 # An input string is valid if:
