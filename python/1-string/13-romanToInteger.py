d5a834d6b8f9fe3e3cac489780b664d3ace89233
Brian Ly <brian.ly@mnltechnology.com>
2020-01-21 22:39:05 +0700

    Commit 98-Validate BST

--- python/1-string/13-romanToInteger.py
+++ python/1-string/13-romanToInteger.py
@@ -26,8 +26,7 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         number = 0
         i = 0
-        Dict = {"I": 1, "V": 5, "X": 10, "L": 50,
-                "C": 100, "D": 500, "M": 1000}
+        Dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 
         while i < len(s) - 1:
             n1 = Dict[s[i]]
