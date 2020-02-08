14a6ac9596d5629f375f6e1cdc9c411b112a89c6
Brian Ly <brian.ly@mnltechnology.com>
2020-02-08 21:22:44 +0700

    Updated codes

--- python/2-array/H4-findMedianSortedArrays.py
+++ python/2-array/H4-findMedianSortedArrays.py
@@ -14,8 +14,15 @@
 # nums2 = [3, 4]
 # The median is (2 + 3)/2 = 2.5
 
-# TODO: Approach 1: Using sorted()          [O(NlogN) & O(1)]
-# TODO: Approach 2: Recursive Approach      [O(log(min(m,n))) & O[1]]
+# TODO: Approach 1: Using sorted()                      [O(NlogN) & O(N)]
+# TODO: Approach 2: Using Interative Solution           [O(log(min(x,y))) & O[1]]
+# Time complexity: O(log(min(x, y))), where x and y are lengths of nums1 & nums2.
+# 1. Pick the shorter array to binary search on, as it would take more time to partition the longer array.
+# 2. Set max and min for left and right partitions on both arrays (set to -inf and +inf when partition is empty).
+# 3. If all elements on the arrays on the left are smaller than those on the right, return the median based on the parity of combined length of the input arrays.
+# 4. If we cut the partition too deep into the right side, move the pivot toward the left. 
+#       And vice versa until we reach a point where all elements in left partitions are smaller than all elements in the right partitions.
+
 
 from typing import List
 
@@ -25,37 +32,40 @@ class Solution:
         nums = sorted(nums1 + nums2)
         len12 = len(nums)
 
-        if len12 % 2:
+        if len12 % 2:  # ~~ len12 % 2 != 0
             return (nums[len12//2])
         else:
             return (nums[len12//2] + nums[len12//2 - 1]) / 2
 
-
     def findMedianSortedArray2(self, nums1: List[int], nums2: List[int]) -> float:
         len1, len2 = len(nums1), len(nums2)
 
+        # We need to have nums1 as smaller in length
         if len1 > len2:
             return self.findMedianSortedArray2(nums2, nums1)
 
         low, high = 0, len1
         while low <= high:
-            partition1 = (low + high) // 2
-            partition2 = (len1 + len2 + 1) // 2 - partition1
+            part1 = (low + high) // 2
+            part2 = (len1 + len2 + 1) // 2 - part1
 
-            maxLeft1 = nums1[partition1 - 1] if partition1 != 0 else float("-inf")
-            minRight1 = nums1[partition1] if partition1 != len1 else float("inf")
-            maxLeft2 = nums2[partition2 - 1] if partition2 != 0 else float("-inf")
-            minRight2 = nums2[partition2] if partition2 != len2 else float("inf")
+            maxLeft1 = nums1[part1 - 1] if part1 != 0 else float("-inf")
+            minRight1 = nums1[part1] if part1 != len1 else float("inf")
+            maxLeft2 = nums2[part2 - 1] if part2 != 0 else float("-inf")
+            minRight2 = nums2[part2] if part2 != len2 else float("inf")
 
-            if maxLeft1 <= minRight2 and maxLeft2 <= minRight1:
-                if (len1 + len2) % 2:
+            if (maxLeft1 <= minRight2 and
+                    maxLeft2 <= minRight1):
+
+                if (len1 + len2) % 2:   # ~~ (len1 + len2) % 2 != 0:
                     return max(maxLeft1, maxLeft2)
                 else:
                     return (max(maxLeft1, maxLeft2) + min(minRight1, minRight2)) / 2
+
             elif maxLeft1 > minRight2:
-                high = partition1 - 1
+                high = part1 - 1
             else:
-                low = partition1 + 1
+                low = part1 + 1
 
 
 if __name__ == "__main__":
@@ -65,8 +75,14 @@ if __name__ == "__main__":
     # nums1 = [1, 2]
     # nums2 = [3, 4]
 
+    # nums1 = [1, 2, 3]
+    # nums2 = [4, 5]
+
+    # nums1 = [2, 4, 6]
+    # nums2 = [1, 3, 5]
+
     nums1 = [1, 2, 3]
-    nums2 = [4, 5]
+    nums2 = [4, 5, 10]
 
     s = Solution()
     # print(s.findMedianSortedArray1(nums1, nums2))
